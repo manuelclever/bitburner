d26bf2bede9906a5b200d50fddbb0f06f07ca030
manuel <manuel-clever@web.de>
2022-01-23 15:52:06 +0100

    running

--- src/run.js
+++ src/run.js
@@ -3,11 +3,11 @@ import {findHackable} from "controlCenter.js";
 import {hackServer} from "controlCenter.js";
 
 export async function main(ns) {
-    ns.run("hx_n00dles.js");
-    ns.run("hx_foodnstuff.js");
-    ns.run("hx_sigma-cosmetics.js")
-    ns.run("hx_joesguns.js");
-    ns.run("hx_hong-fang-tea.js");
-    ns.run("hx_harakiri-sushi.js");
-    ns.run("hx_iron-gym.js");
+    await ns.run("/hx/n00dles.js");
+    await ns.run("/hx/foodnstuff.js");
+    await ns.run("/hx/sigma-cosmetics.js")
+    await ns.run("/hx/joesguns.js");
+    await ns.run("/hx/hong-fang-tea.js");
+    await ns.run("/hx/harakiri-sushi.js");
+    await ns.run("/hx/iron-gym.js");
 }
