b0431929058f80c09a0c71aa9076bf87a63e0d62
manuel <manuel-clever@web.de>
2022-01-23 15:12:02 +0100

    directories

---
+++ src/download.js
@@ -0,0 +1,16 @@
+/** @param {NS} ns **/
+
+export async function main(ns) {
+    ns.wget();
+    ns.wget();
+    ns.wget();
+    ns.wget();
+    ns.wget();
+    ns.wget();
+    ns.wget();
+    ns.wget();
+    ns.wget();
+    ns.wget();
+    ns.wget();
+    ns.wget();
+}
