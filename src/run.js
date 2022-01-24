7cbc6b453b1ad5959674c0611e51af2dbff85b66
manuel <manuel-clever@web.de>
2022-01-24 18:54:47 +0100

    run.js with threads

--- src/run.js
+++ src/run.js
@@ -3,14 +3,16 @@ import {findHackable} from "controlCenter.js";
 import {hackServer} from "controlCenter.js";
 
 export async function main(ns) {
-    
+    const fileRam = ns.getScriptRam("/hx/n00dles.js");
+    const serverRam = ns.getServerMaxRam("xtra");
+    const files = ["/hx/n00dles.js", "/hx/foodnstuff.js", "/hx/sigma-cosmetics.js",
+                    "/hx/joesguns.js", "/hx/hong-fang-tea.js", "/hx/harakiri-sushi.js",
+                    "/hx/iron-gym.js"];
 
+    ns.tprint(serverRam + "/" + fileRam + "/" + files.length);
+    const threadsPerFile = (serverRam / fileRam) / files.length;
 
-    await ns.run("/hx/n00dles.js");
-    await ns.run("/hx/foodnstuff.js");
-    await ns.run("/hx/sigma-cosmetics.js")
-    await ns.run("/hx/joesguns.js");
-    await ns.run("/hx/hong-fang-tea.js");
-    await ns.run("/hx/harakiri-sushi.js");
-    await ns.run("/hx/iron-gym.js");
+    for(var i = 0; i < files.length; i++) {
+        await ns.run(files[i], threadsPerFile);
+    }
 }
