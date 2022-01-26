80c4710a6df3b93d54808f78d041d12f5b277a26
manuel <manuel-clever@web.de>
2022-01-26 18:53:29 +0100

    reads from txt file

--- src/serverInfos.js
+++ src/serverInfos.js
@@ -1,26 +1,32 @@
 /** @param {NS} ns **/
-import {findHackable} from "controlCenter.js";
+import { findHackable } from "controlCenter.js";
 
 export async function main(ns) {
-	var allServers = ns.scan("home");
 
-	for(var i = 0; i < allServers.length; i++) {
-		var server = allServers[i];
+	let myPromise = findHackable(ns);
+	myPromise.then(function (hackableServers) {
+		var targets = hackableServers;
 
-		var money = ns.getServerMoneyAvailable(server);
-		var maxMoney = ns.getServerMaxMoney(server);
-		var moneyPercentage =  Math.round((money / maxMoney) * 100);
-		var rootAccess = ns.hasRootAccess(server);
-		var security = Math.round(ns.getServerSecurityLevel(server));
-		var hackingTime = Math.round(ns.getHackTime(server) / 1000);
-		var anaylzeChance = Math.round(ns.hackAnalyzeChance(server) * 100);
+		for (var i = 0; i < targets.length; i++) {
+			var server = targets[i];
 
-		ns.tprint(server + ":");
-		ns.tprint("\troot: " + rootAccess + 
-			"\n\t\t\tmoney: " + money + " (" + moneyPercentage + "%)" +
-			"\n\t\t\tmaxMoney: " + maxMoney +
-			"\n\t\t\tsecurity: " + security +
-			"\n\t\t\thackTime: ~ " + hackingTime + "s" +
-			"\n\t\t\thackChance: ~ " + anaylzeChance + "%");
-	}
+			var money = ns.getServerMoneyAvailable(server);
+			var maxMoney = ns.getServerMaxMoney(server);
+			var moneyPercentage = Math.round((money / maxMoney) * 100);
+			var rootAccess = ns.hasRootAccess(server);
+			var security = Math.round(ns.getServerSecurityLevel(server));
+			var hackingTime = Math.round(ns.getHackTime(server) / 1000);
+			var anaylzeChance = Math.round(ns.hackAnalyzeChance(server) * 100);
+
+			ns.tprint(server + ":");
+			ns.tprint("\troot: " + rootAccess +
+				"\n\t\t\tmoney: " + money + " (" + moneyPercentage + "%)" +
+				"\n\t\t\tmaxMoney: " + maxMoney +
+				"\n\t\t\tsecurity: " + security +
+				"\n\t\t\thackTime: ~ " + hackingTime + "s" +
+				"\n\t\t\thackChance: ~ " + anaylzeChance + "%");
+		}
+	}, function (error) {
+		ns.tprint("Error. Couldn't get Servers.");
+	});
 }
