b9ffd1315eb38a433fe816223d56fe6f44b4b41c
manuel <manuel-clever@web.de>
2022-01-27 17:33:06 +0100

    purchasServers.js

---
+++ src/purchaseServers.js
@@ -0,0 +1,21 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+	var boughtServ = ns.purchaseServer("serv", 1024);
+
+	const myMoney = ns.getServerMoneyAvailable("home");
+
+	for (var ram = ns.getPurchasedServerMaxRam(); ram > 1; ram / 2) {
+		const serverLimit = ns.getPurchasedServerLimit();
+		
+		if(myMoney > (serverLimit * ns.getPurchasedServerCost(ram))) {
+			
+			await ns.write("myserv.txt", boughtServ + "", "w");
+			for(var i = 0; i < serverLimit; i++) {
+				ns.purchaseServer("serv", ram);
+				await ns.write("myserv.txt", boughtServ + "\n", "a");
+			}
+		}
+	}
+	
+
+}
